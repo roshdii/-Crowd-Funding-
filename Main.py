60ec0c4adfff249f6eaa350f360760f106f7a376
roshdii <32249135+roshdii@users.noreply.github.com>
2019-11-26 13:14:33 +0200

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env/ python3.6
+#! /usr/bin/env python3.6
 ## File : Crowd Funding Donating System 
 ## Author : Eslam Roshdi 
 ## Email : e.roshdii@gmail.com
@@ -6,9 +6,22 @@
 ## Info : e.g kick starter 
 ## Course : Intro to python
 ## Instructor : Abd Arahman Hamdy 
-
+import sys 
+print(sys.version)
+# import cv2
+# print(cv2.__version__)
 import os 
-from menu_class import Menu
+from checking_funcs import *
+from menu_class import MENU
+
+db_dir = "db"
+user_file = "user.csv"
+proj_file = "proj.csv"
+dont_file = "dont.csv"
+
+user_file_header = ['id','name','email','pswd']
+proj_file_header = ['id','name','u_id','total','donats','st_date','end_date','category','details','tags']
+dont_file_header = ['id','p_id','u_id','amount']
 
 class User:
 	# List of Users
@@ -16,46 +29,140 @@ class User:
 
 	users = []
 	no_of_users = 0
+	menu = MENU(['View Info','Edit info','Delete Account'])
+	edit_menu = MENU(['name','email','password'])
 
-	def __init__(self,user_id=no_of_users+1,):
-		pass  
+	def __init__(self,id,name,email,pswd):
 		# check values & assign
 		# add(self)
-
-	def add(self):
-		pass
+		print("construct user")
+		self.id = id  
+		self.name = name 
+		self.email = email 
+		self.pswd = pswd
+		self.projects = []
+		self.donations = []
+	
+	@classmethod
+	def add(cls,**kwargs):
 		# append(self) to list 
 		# recursive update 
+		print("user add")
+		if len(kwargs) == 0 :
+			tmp_dic = {
+				'id' : (cls.no_of_users+1) ,
+				'name' : input("Enter name : "),
+				'email' : input("Enter email : "),
+				'pswd' : input("Enrer password : "),
+				# 'age' : input("Enter age")
+				# 'gender' : input("Enter gender")
+				# 'counter' : input ("Enter country")
+			}
+		else :
+			tmp_dic = kwargs 
+
+		for obj in cls.users :
+			if obj.email == tmp_dic['email'] :
+				return -1 
+
+		X = cls(**tmp_dic) ; 
+		cls.users.append(X)
+		cls.no_of_users += 1
+		cls.recursive_update_users()
+		return X 
+	
 
 	def edit(self):
-		pass
+		print("user edit")
 		# display all info
 		# edit whatever 
 		# recursive update 
+		while True :
+			clear_screen()
+			self.view()
+			x = User.edit_menu.draw()
+			if x == 1 :
+				self.name = input("Enter new name : ")
+			elif x == 2 : 
+				self.email = input("Enter new email : ")
+				# consider change to a preserved one 
+			elif x == 3 : 
+				self.pswd = input("Enter new password : ")
+			elif x == 0 :
+				break ;
+			else : 
+				print("invalid")
+		User.recursive_update_users()
 
 	def delete(self):
-		pass
+		print("user delete")
 		# remove user obj from list 
 		# recursice update 
+		if input("Are you sure to delete you Account ?[Y/N]") == 'Y' :
+			x = input("Enter your password : ")
+			if x == self.pswd :
+				User.users.remove(self)
+				cls.recursive_update_users()
+				input("Account Removed")
+
+			else :
+				input("Invalid password ")
 
 	def view(self):
-		pass
+		print("user view")
 		# display current user info
+		print("ID : ",self.id)
+		print("Name : ",self.name)
+		print("Email : ",self.email)
 
+
+	@classmethod
+	def login(cls,email,psw):
+		print("user select")
+		# search for user with email 
+		# check for psw 
+		# return user obj if ok 
+		# return -1 if not ok
+		for obj in cls.users:
+			if obj.email == email and obj.pswd == psw :
+					return obj
+		return -1 
 	########### FILE ######################
 	@classmethod
 	def recursive_read_users(cls):
-		pass 
+		print("users recursive read ")
+		# check db file is exit if not make a new 
 		# read users file db 
 		# convert to user obj 
 		# append obj to users list 
+		check_file(db_dir,user_file,user_file_header)
+		with open(db_dir+'/'+user_file,'r') as file :
+			txt_reader = csv.DictReader(file)
+			for line in txt_reader :
+				usr = dict(line)
+				cls.add(**usr)
 
 	@classmethod
-	def recursve_update_users(cls):
-		pass
+	def recursive_update_users(cls):
+		print("users recursive update ")
+		# check db file is exit if not make a new 
 		# loop users list 
-		# convert from obj to struct 
+		# convert from obj to dict 
 		# write to users file 
+		check_file(db_dir,user_file,user_file_header)
+		with open(db_dir+'/'+user_file,'w') as file :
+			txt_writer = csv.DictWriter(file,fieldnames=user_file_header)
+			txt_writer.writeheader()
+			for obj in cls.users:
+				tmp_dic = {
+					'id' : obj.id,
+					'name':obj.name,
+					'email':obj.email,
+					'pswd':obj.pswd,
+				}
+				txt_writer.writerow(tmp_dic)
+
+
 
 class Project:
 	# List of Projects 
@@ -64,116 +171,323 @@ class Project:
 
 	projects = []
 	no_of_projects = 0 
+	menu = MENU(['Add Project','donate projct','Edit Project','Delete Project','View project','View Project of user','View Projects by date','View All Projects'])
 
-	def __init__(self,project_id=no_of_projects+1,user_id):
-		pass
+	proj_menu = MENU(projects)
+
+	def __init__(self,id,name,u_id,total,donats,start,end,tags,details,category):
+		print("construct project ")
 		# check_values & assign them 
 		# start project 
+		self.id = int(id)
+		self.name = name 
+		self.u_id = int(u_id)
+		self.total = int(total)
+		self.donats = int(donats) 
+		self.st_date = start
+		self.end_date = end 
+		self.category = category 
+		self.details = details 
+		self.tags = tags 
 
-	def start(self):
-		pass
-		# append(self) in list
-		# recursive update project file
+	@classmethod
+	def add(cls,**kwargs):
+		print("add project ")
+		# take values 
+		# append to list 
+		# recursive update project file 
+		 
+		for obj in cls.projects :
+			if obj.id == kwargs['id'] :
+				input("Project found before !!")
+				return -1 
+
+		X = cls(**kwargs)
+		cls.projects.append(X)
+		cls.no_of_projects += 1 
+		cls.recursve_update_projects()
+		cls.menu_update()
+		return X 
+
+	@classmethod
+	def start(cls,u_id,name=None):
+		print("project start")
+		# recieve info about project from user 
+		if name == None : 
+			name = input("Enter projct name : ") 
+
+		tmp_dic = {
+			'id' : cls.no_of_projects+1 ,
+			'name' : name ,
+			'u_id' : u_id,
+			'total' : int(input("Enter total targer : ")) ,
+			'donats' : 0 ,
+			'st_date':input("Enter start date of campaign : "),
+			'end_date':input("Enter end date of campaign : "),
+			'category':input("Enter Category : "),
+			'details':input("Enter details of project : "),
+			'tags':input("Enter tags : "),
+
+		}
+		# return tmp_dic 
+		return cls.add(**tmp_dic)
+	
+	@classmethod 
+	def search_by_user(cls,u_id):
+		print("project select ")
+		lis = []
+		for pro in cls.projects : 
+			if pro.u_id == u_id :
+				print(pro.name)
+				lis.append(pro)
+		return lis  
+
+	@classmethod 
+	def search_by_id(cls,id_):
+		print("project select ")
+		lis = []
+		for pro in cls.projects : 
+			if pro.id == id_ :
+				# print(pro.name)
+				lis.append(pro) 
+		return lis 
+
+	@classmethod 
+	def search_by_time(cls,st_time,end_time):
+		print("project select ")
+		# for pro in projects : 
+		# 	if pro.u_id = u_id :
+		# 		print(pro.name)
+		# 		return pro 
+		lis = []
+
+		return lis 
+
+	def donate(self):
+		print("project donate")
+		# construct donate with id of user & project
+		print("Current total donation of project : ",self.total," Needed donations = ",self.donats)
+		x= input("Enter donation value : ")
+		try :
+			if x.isdigit() :
+				self.total = int(self.total) +  int(x) 
+				Project.recursve_update_projects()
+			else : 
+				input("Not digit ")
+		except :
+			input("Error enter value ")
+
+	def edit(self):
+		print("project edit")
+		# edit details of project 
+		# recursive update 
+		print("Current name of project :",self.name)
+		self.name = input("Enter the new name : ")
+		Project.recursve_update_projects()
 
 	def end(self):
-		pass
+		print("project end")
 		# check 25% off and time not passed
 		# remove project from list
 		# recursive update project file
-	
+		if input("Are your sure to end this project ? [Y/N]")  == 'Y' :
+			Project.projects.remove(self)
+			Project.no_of_projects -= 1 
+			Project.recursve_update_projects()
+			Project.menu_update()
+
+	def view(self):
+		print("projct view")
+		# print all attribuites of this project 
+		print("ID : ",str(self.id))
+		print("Name : ",self.name)
+		print("User/Creator ID : ", str(self.u_id))
+		print("User/Creator Name : " , User.users[int(self.u_id)-1].name )
+		print("Total needed donations : ",int(self.donats))
+		print("Current donats : ",int(self.total))
+
+
 	###### Out Sourceing Searching #######
 	@classmethod
 	def projects_of_user(cls,user_id):
-		pass 
+		print("projects of user")
 		# display all projects for specific user
+		for pro in cls.projects :
+			if pro.u_id == user_id :
+				pro.view()
 
 	@classmethod
-	def projects_of_time(cls,st_date,end_date):
-		pass
+	def projects_of_time(cls,search_st_date):
+		print("projects of time ")
 		# display all projects(may inc. donats) within time search 
-
+		s = st_date.split('-')
+		# e = end_date.split('-')
+		s = [int(i) for i in s]
+		# e = [int(i) for i in e]
+		s = [(s[0]+s[1]*30),s[2]]
+		# e = [(e[0]+e[1]*30),e[2]] 
+		for pro in cls.projects :
+			p_st = [pro.st_date[0]+ pro.st_date[1]*30 , pro.st_date[2]]
+			p_en = [pro.end_date[0]+ pro.end_date[1]*30 , pro.end_date[2]]
+			if s > p_st and s < p_en :
+				print(pro.name) 
+			
 	@classmethod
 	def all_projects(cls):
-		pass
+		print("all projects ")
 		# display all projects 
+		for pro in cls.projects : 
+			pro.view()
 
 	########### FILE ######################
 	@classmethod
 	def recursive_read_projects(cls):
-		pass 
+		print("projects recursive read")
+		# check db file is exit if not make a new 
 		# read projects file db 
 		# convert to project obj 
 		# append obj to projects list 
+		check_file(db_dir,proj_file,proj_file_header)
+		with open(db_dir+'/'+proj_file,'r') as file :
+			txt_reader = csv.DictReader(file)
+			for line in txt_reader :
+				pro = dict(line)
+				cls.add(**pro)
 
 	@classmethod
 	def recursve_update_projects(cls):
-		pass
+		print("projects recursive update")
+		# check db file is exit if not make a new 
 		# loop projctss list 
 		# convert from obj to struct 
 		# write to projects file 
-
-
-class Donation:
-	# List of donations 
-	# add a donate with the project id & user id donating 
-	# remove a donate (at specfic project / from login user)
+		check_file(db_dir,proj_file,user_file_header)
+		with open(db_dir+'/'+proj_file,'w') as file :
+			txt_writer = csv.DictWriter(file,fieldnames=proj_file_header)
+			txt_writer.writeheader()
+			for obj in cls.projects:
+				tmp_dic = {
+					'id' : obj.id,
+					'name':obj.name,
+					'u_id':obj.u_id,
+					'total':obj.total,
+					'donats':obj.donats,
+				}
+				txt_writer.writerow(tmp_dic)
 	
-	dontas = []
-	no_of_donats = 0 
-
-	def __init__(self,donate_id=no_of_donats+1,user_id,project_id):
-		pass 
-		# check & assign values to current donation
-		# add (self)
-
-	def add(self):
-		pass
-		# append donate_obj to list 
-		# recursive update donate file 
-
-	def remove(Self):
-		pass
-		# remove donate with id from list 
-		# recursive update donate file
-
-
-	########## out sourceing search ####################
-	@classmethod 
-	def donats_of_user(cls,user_id):
-		pass
-		#search for all donats with user_id 
-		#display donats and prespective projects
-
 	@classmethod
-	def donats_of_project(cls,project_id):
-		pass
-		#search for all donats with project_id 
-		#display donats and prespective user 
+	def menu_update(cls):
+		cls.proj_menu.edit(cls.projects)
 
-	########### FILE ######################
-	@classmethod
-	def recursive_read_donats(cls):
-		pass 
-		# read donate file db 
-		# convert to donate obj 
-		# append obj to donats list 
-
-	@classmethod
-	def recursve_update_donats(cls):
-		pass
-		# loop donats list 
-		# convert from obj to struct 
-		# write to donate file 
-
-def Signup:
-	pass
+def Signup():
+	print("Sign up ")
 	#1 Get Info from user(check if valid) 
 	#2 Add USer (check return is ok)
 	#3 Re #1 if not ok 
 	#4 Back to main 
+	if User.add() == -1 :
+		input("User found before !! [try to login]")
+	else :
+		input("Sucessfully SignUp .. try to login")
+	
+def Login():
+	print("Log in")
+	l_menu = MENU([])
+	errs = 0 
+	
+	while True :
+		clear_screen()
+		email = input("Enter your valid email : ")
+		ps = input("Enter your password : ")
+		loged_user = User.login(email,ps)
+		if loged_user == -1 :
+			errs += 1 
+			if errs >= 3 :
+				input("3 Invalid Attempts :: Account Locked !!! ")
+				return -1 ; 
+			if '0' == input("Invalid login [press to try agian /or/ 0 to exit] : "+"\n"):
+				return 0
+		else :
+			input("Successfully login Hi "+loged_user.name)
+			break 
 
-def Login:
-	pass
+	
+	while True :			
+		clear_screen()
+		re1 = acco_menu.draw()
+		if re1 == 1 :
+			while True :
+				clear_screen()
+				re2 = User.menu.draw()
+				if re2 == 1 :
+					loged_user.view()
+					input("Enter any key to continue ")
+				elif re2 == 2 :
+					loged_user.edit()
+				elif re2 == 3 :
+					loged_user.delete()
+					return ;
+				elif re2 == 0 :
+					break;
+				else :
+					print("Invalid ")
+
+		elif re1 == 2 :
+			
+			while True :
+				clear_screen()
+				re2 = Project.menu.draw()
+				if re2 == 1 :
+					ProX = Project.start(loged_user.id)
+					continue ; 
+				elif re2 == 6 :
+					ss = input("Enter user id : [0 for current user ]")
+					try :
+						if ss.isdigit()  :
+							if ss == 0 : ss = loged_user.id 
+							Project.projects_of_user(int(ss))
+							input()
+					except :
+						input("Invalid user id ")
+					continue ;
+				elif re2 == 7 : 
+					ss = input("Enter start date [dd-mm-yyyy]")
+					Project.projects_of_time(ss)
+					input()
+					continue ;
+				elif re2 == 8 :
+					Project.all_projects()
+					input()
+					continue ;
+				elif re2 == 0 :
+					break;
+
+				l = Project.search_by_user(int(loged_user.id))
+				clear_screen()
+				l_menu.edit([i.name for i in l])
+				x = l_menu.draw()
+
+				if x == 0 or x == -1 :
+					continue 
+
+				if re2 == 2 :
+					l[x-1].donate()
+				elif re2 == 3 :
+					l[x-1].edit()
+				elif re2 == 4 : 
+					l[x-1].end()
+				elif re2 == 5 :
+					l[x-1].view()
+					input()
+				else :
+					print("Invalid ") 
+
+		elif re1 == 3 :
+			print("Sign out")
+			break;
+
+		else :
+			print("invalid")
 	#1 check email/pass for user >> if true login_user is selected  
 	#2 User menu : Account settings , Projects , Donations , Signout
 	#3 Account Setting menu : Edit Info , Delete Account 
@@ -181,24 +495,109 @@ def Login:
 	#5 Donation Menu : project list (donate project / delete donate )
 	#6 Sign Out 
 
-def main:
-	pass
+main_menu = MENU(['Signup','Login','Exit'])
+acco_menu = MENU(['Account','Projects','Signout'])
+
+def main():
+	
 	#1 main menu : Signup , login 
 	#2 signup > go for signup 
 	#3 login > go for login 
-	#4 Exit
+	#4 Exit 
+	check_dir(db_dir)
+	User.recursive_read_users()
+	Project.recursive_read_projects()
 
+	while True :
+		clear_screen()
+		re1 = main_menu.draw()
+		if re1 == 1 :
+			Signup()
+		elif re1 == 2 :
+			if Login() == -1 :
+				return -1
+		elif re1 == 3 :
+			return 0 ; 
+		else :
+			print("Invalid")
 
 if __name__ == "__main__":
 	main()
 
+# class Donation:
+# 	# List of donations 
+# 	# add a donate with the project id & user id donating 
+# 	# remove a donate (at specfic project / from login user)
+	
+# 	dontas = []
+# 	no_of_donats = 0 
+# 	menu = MENU(['add','donates of user','donats on project'])
 
-##############################################################
-def check_dir(cls):
-	try :
-		l = os.listdir(db_dir_name)
-		if len(l) > 0 :
-			print("Done checking db/ no. of files = ",len(l))
-	except FileNotFoundError:
-		print("Error in accessing Database directory !!! Creating New db ")
-		os.mkdir(db_dir_name)
+# 	def __init__(self,donate_id,project_id,user_id,total_donat):
+# 		print("construct donate")
+# 		if donate_id == None :
+# 			donate_id = no_of_donats + 1  
+# 		# check & assign values to current donation
+# 		# add (self)
+# 		self.id = donate_id 
+# 		self.u_id = user_id 
+# 		self.p_id = project_id 
+# 		self.total_donates = self.total_donates  + total_donat
+
+# 	@classmethod
+# 	def add(cls,d_id,p_id,u_id,total_dona):
+# 		print("donat add ")
+# 		# append donate_obj to list 
+# 		# recursive update donate file 
+# 		x = cls(d_id,p_id,u_id,total_dona)
+# 		cls.donates.append(x)
+# 		Donation.recursve_update_donats()
+
+# 	def remove(Self):
+# 		print("donat remove")
+# 		# remove donate with id from list 
+# 		# recursive update donate file
+
+
+# 	########## out sourceing search ####################
+# 	@classmethod 
+# 	def donats_of_user(cls,user_id):
+# 		print("donats of user")
+# 		#search for all donats with user_id 
+# 		#display donats and prespective projects
+
+# 	@classmethod
+# 	def donats_on_project(cls,project_id):
+# 		print("donats on project")
+# 		#search for all donats with project_id 
+# 		#display donats and prespective user 
+
+# 	########### FILE ######################
+# 	@classmethod
+# 	def recursive_read_donats(cls):
+# 		print("donats recursrive read")
+# 		# check db file is exit if not make a new 
+# 		# read donate file db 
+# 		# convert to donate obj 
+# 		# append obj to donats list 
+
+# 	@classmethod
+# 	def recursve_update_donats(cls):
+# 		print("donats recursrive update ")
+# 		# check db file is exit if not make a new 
+# 		# loop donats list 
+# 		# convert from obj to struct 
+# 		# write to donate file 
+# 		check_file(db_dir,donat_file,donat_file_header)
+# 		with open(db_dir+'/'+donat_file,'w') as file :
+# 			txt_writer = csv.DictWriter(file,fieldnames=donat_file_header)
+# 			txt_writer.writeheader()
+# 			for obj in cls.donates:
+# 				tmp_dic = {
+# 					'id' : obj.id,
+# 					'u_id':obj.name,
+# 					'p_id':obj.u_id,
+# 					'total':obj.total,
+# 					'donats':obj.donats,
+# 				}
+# 				txt_writer.writerow(tmp_dic)
--- menu_class.py
+++ menu_class.py
@@ -14,12 +14,14 @@ class MENU :
 			print(str(idx)+"- "+i)
 			idx+= 1
 		print(self.closing_______line)
+		return self.get()
 
 	def get(self):
 		print("Enter no. or 'identical' Name of item to choose [0 to exit menu ]")
 		print(self.closing_______line)
 		x = input("-> ")
 		# print(str(type(x)) == "<class 'str'>")
+		if x == '' : return -1 
 		if x == 0 or x == '0' : return 0 
 		elif x.isdigit() : 
 			x = int(x) 
@@ -40,7 +42,8 @@ class MENU :
 					# print("i/p = ",x)
 					print("-> Selected Element [",self.inlist[idx-1]," ]")
 					print(self.closing_______line)
-					return str(x)
+					# return str(x)
+					return idx
 				idx += 1 
 		else :
 			input("Invalid input for menu items") 
